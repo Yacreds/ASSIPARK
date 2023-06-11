44f616e1c9f392b0c022420a85be5b288dd7a7a3
Kevin Carrillo <37084646+kcarrillor01@users.noreply.github.com>
2023-06-11 13:57:34 -0500

    Implementar css, js e imagenes

--- ASSIPARK/settings.py
+++ ASSIPARK/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+STATICFILES_DIRS=['ASSIPARK_APP\static']
 # Application definition
 
 INSTALLED_APPS = [
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
